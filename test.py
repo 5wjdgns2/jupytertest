dec633b209f87d5e5402f28a0773573bac212460
5wjdgns2 <61228399+5wjdgns2@users.noreply.github.com>
2020-06-11 15:24:34 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+echo "# jupytertest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/5wjdgns2/jupytertest.git
+git push -u origin master
+                
